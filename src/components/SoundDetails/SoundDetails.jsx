2e1d6f2d94115ca49a55ed9dfd21f34131905cd9
danny amit <danny@iamit.org>
2025-10-07 10:38:47 -0400

    update

--- src/components/SoundDetails/SoundDetails.jsx
+++ src/components/SoundDetails/SoundDetails.jsx
@@ -5,6 +5,7 @@ import { getSoundById } from "../../services/soundService.js";
 import CommentForm from "../CommentForm/CommentForm.jsx";
 import CommentList from "../CommentList/CommentList.jsx";
 import AudioPlayer from "../AudioPlayer/AudioPlayer.jsx";
+import "./SoundDetails.css";
 
 function SoundDetails() {
   const { soundId } = useParams();
@@ -26,11 +27,13 @@ function SoundDetails() {
   }, []);
 
   const audioSrc = sound?.fileId
-    ? `${import.meta.env.VITE_BACK_END_SERVER_URL}/sounds/stream/${sound.fileId}`
+    ? `${import.meta.env.VITE_BACK_END_SERVER_URL}/sounds/stream/${
+        sound.fileId
+      }`
     : null;
 
   return (
-    <>
+    <div className="sound-details">
       <h1>{sound && sound.title}</h1>
       {sound && (
         <AudioPlayer
@@ -48,10 +51,15 @@ function SoundDetails() {
         />
       )}
       <div>
-        <CommentForm soundId={soundId} onCommentAdded={(newComment) => setComments([...comments, newComment])} />
+        <CommentForm
+          soundId={soundId}
+          onCommentAdded={(newComment) =>
+            setComments([...comments, newComment])
+          }
+        />
         <CommentList comments={comments} />
       </div>
-    </>
+    </div>
   );
 }
 
